356ff313c046ef60ad829dc2a7861b4439d90e46
tecnologia_vhdl <ramiro.ramirocarpio@gmail.com>
2026-03-20 18:59:17 -0300

    actualice codigo, parte de los vectores en estimulos y definición de valores

--- mc74_tb.vhd
+++ mc74_tb.vhd
@@ -10,7 +10,7 @@ architecture test_mc74 of mc74_tb is
 	--Declaración de componentes
 	component mc74
 	port(
-		A0,A1,A3,A4,A5,A6,A7 : in std_logic;
+		A0,A1,A2,A3,A4,A5,A6,A7 : in std_logic;
         sel : in std_logic_vector(2 downto 0);
         G : in std_logic; --enable activo en bajo
         Y, y : out std_logic -- Y:salida normal - y: salida invertida
@@ -18,8 +18,8 @@ architecture test_mc74 of mc74_tb is
 	end component;
 	
 	--Declaración de señales
-	signal t_A0 ,t_A1,t_A2,t_A3,t_A4, t_A5, t_A6, t_A7 : std_logic;
-	signal t_sel : std_logic_vector(1 downto 0);
+	signal t_A0 ,t_A1,t_A2,t_A3,t_A4, t_A5, t_A6, t_A7 : std_logic := '0';
+	signal t_sel : std_logic_vector(2 downto 0) := "000";
     signal t_G : std_logic := '1';
     signal t_Y, t_y : std_logic;
 
@@ -47,10 +47,51 @@ begin
 	--Estímulos
 	estimulos: process
 	begin
-        G <= "1";
-        D <= "10101010";
+        --Deshabilitación
+        t_G <= '1';
         wait for 20ns;
+        --habilitación
+        t_G <= '0'
+        
+        tb_A0 <= '1';
+        t_sel <= "000";
+        wait for 20 ns;
+        tb_A0 <= '0';
 
+        tb_A1 <= '1';
+        t_sel <= "001";
+        wait for 20 ns;
+        tb_A1 <= '0';
+
+        tb_A2 <= '1';
+        t_sel <= "010";
+        wait for 20 ns;
+        tb_A2 <= '0';
+
+        tb_A3 <= '1';
+        t_sel <= "011"; 
+        wait for 20 ns;
+        tb_A3 <= '0';
+
+        tb_A4 <= '1'; 
+        t_sel <= "100";
+        wait for 20 ns;
+        tb_A4 <= '0';
+
+        tb_A5 <= '1'; 
+        t_sel <= "101"; 
+        wait for 20 ns;
+        tb_A5 <= '0'; 
+
+        tb_A6 <= '1'; 
+        t_sel <= "110"; 
+        wait for 20 ns;
+        tb_A6 <= '0';
+
+        tb_A7 <= '1'; 
+        t_sel <= "111"; 
+        wait for 20 ns;
+        tb_A7 <= '0'; 
 	end process;
 
 end architecture test_mc74;

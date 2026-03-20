a6427e03e03e68da2bd48ace8445dfe3ba32643f
tecnologia_vhdl <ramiro.ramirocarpio@gmail.com>
2026-03-20 19:13:36 -0300

    corregi que me faltaba A2, y puse en uso G ya lo declare y no use

--- mc74.vhd
+++ mc74.vhd
@@ -4,7 +4,7 @@ use IEEE.numeric_std.all;
 
 entity M74HC251 is
   port (
-    A0,A1,A3,A4,A5,A6,A7 : in std_logic;
+    A0,A1,A2,A3,A4,A5,A6,A7 : in std_logic;
     sel : in std_logic_vector(2 downto 0);
     G : in std_logic; --enable activo en bajo
     Y, y : out std_logic -- Y:salida normal - y: salida invertida
@@ -15,28 +15,32 @@ architecture beh of M74HC251 is
 begin
   
   --proceso secuencial
-  process(A0,A1,A3,A4,A5,A6,A7,sel, G)
+  process(A0,A1,A2,A3,A4,A5,A6,A7,sel, G)
   begin
-    case sel is
-      when "000" =>
-        Y <= A0;
-      when "001" =>
-        Y <= A1;
-      when "010" =>
-        Y <= A2;
-      when "011" =>
-        Y <= A3;
-      when "100"=>
-        Y <= A4;
-      when "101" =>
-        Y <= A5; 
-      when "110" =>
-        Y <= A6;
-      when "111" =>
-        Y <= A7;
-      when others =>
-        null;
-     end case;    
+    if G = '1' then
+      Y <= '0'; -- se encuentra deshabilitado
+    else
+      case sel is
+        when "000" =>
+          Y <= A0;
+        when "001" =>
+          Y <= A1;
+        when "010" =>
+          Y <= A2;
+        when "011" =>
+          Y <= A3;
+        when "100"=>
+          Y <= A4;
+        when "101" =>
+          Y <= A5; 
+        when "110" =>
+          Y <= A6;
+        when "111" =>
+          Y <= A7;
+        when others =>
+          Y <= '0';
+      end case; 
+    end if;   
      y <= not Y; --salida complementaria 
     end process;
        
